1920a8db0558e386197480e49b337c6115c3fdfc
Sankara Paramasivam <sankar.paramasivam@aspiresys.com>
2020-08-06 16:18:23 +0530

    fixed kubernetes setup

--- server.js
+++ server.js
@@ -21,6 +21,13 @@ mongoose.connect(dbConfig.url, {
 
 const app = express()
 app.use(cors())
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }))
 
